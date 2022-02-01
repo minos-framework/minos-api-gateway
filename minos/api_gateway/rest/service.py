49b8ceb8e744d5a036fb1327a5f4ae47c98dfd70
Vladyslav Fenchak <vladyslav.fenchak@clariteia.com>
2022-02-01 21:02:49 +0100

    ISSUE #74

--- minos/api_gateway/rest/service.py
+++ minos/api_gateway/rest/service.py
@@ -1,5 +1,10 @@
 import logging
+from pathlib import (
+    Path,
+)
 
+import aiohttp_jinja2
+import jinja2
 from aiohttp import (
     web,
 )
@@ -61,6 +66,14 @@ class ApiGatewayRestService(AIOHTTPService):
         app.router.add_route("POST", "/admin/rules", AdminHandler.create_rule)
         app.router.add_route("PATCH", "/admin/rules/{id}", AdminHandler.update_rule)
         app.router.add_route("DELETE", "/admin/rules/{id}", AdminHandler.delete_rule)
+
+        # Administration routes
+        aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("minos/api_gateway/rest/backend/templates"))
+        # app.router.add_static('/admin', path='minos/api_gateway/rest/backend', name='static',
+        # follow_symlinks=True, show_index=True)
+        app.router.add_route("*", "/administration", self.handler)
+        app.router.add_route("GET", "/administration/{filename:.*}", self._serve_files)
+
         app.router.add_route("*", "/{endpoint:.*}", orchestrate)
 
         return app
@@ -75,3 +88,38 @@ class ApiGatewayRestService(AIOHTTPService):
 
     async def create_database(self):
         Base.metadata.create_all(self.engine)
+
+    @aiohttp_jinja2.template("tmpl.jinja2")
+    def handler(self, request):
+        context = {}
+        response = aiohttp_jinja2.render_template("index.html", request, context)
+        response.headers["Content-Language"] = "es"
+        return response
+
+    async def _serve_files(self, request: web.Request) -> web.FileResponse:
+        rel_url = request.match_info["filename"]
+
+        path = Path(Path.cwd())
+        self._directory = path.resolve()
+
+        try:
+            filename = Path(rel_url)
+            if filename.anchor:
+                # rel_url is an absolute name like
+                # /static/\\machine_name\c$ or /static/D:\path
+                # where the static dir is totally different
+                raise web.HTTPForbidden()
+            filepath = self._directory.joinpath("minos", "api_gateway", "rest", "backend", "admin", filename).resolve()
+
+            # p = filepath.relative_to(self._directory)
+            return web.FileResponse(path=filepath, status=200)
+
+        except (ValueError, FileNotFoundError) as error:
+            # relatively safe
+            raise web.HTTPNotFound() from error
+        except web.HTTPForbidden:
+            raise
+        except Exception as error:
+            # perm error or other kind!
+            request.app.logger.exception(error)
+            raise web.HTTPNotFound() from error
